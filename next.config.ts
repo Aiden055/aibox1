c9b84e4dc1e5070ed96f0855bc4c43d47fd7c534
Aiden055 <wyu220053@gmail.com>
2024-10-31 14:17:08 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,3 +14,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
